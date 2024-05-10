473c5d1531d5539504f9f185fc8e5564d7dfeaf8
SandaruwanB <sandarusbandara110@gmail.com>
2024-05-10 19:54:09 +0530

    password hashing

--- src/Login.java
+++ src/Login.java
@@ -13,7 +13,11 @@ import javax.swing.JPasswordField;
 import javax.swing.JCheckBox;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.awt.event.ActionEvent;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 
@@ -23,13 +27,15 @@ public class Login extends JFrame {
 	private JPanel contentPane;
 	private JTextField fieldUserName;
 	private JPasswordField passwordField;
-
+	private Dashboard dashboard = new Dashboard();
+	private DbConnection con = new DbConnection();
 
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+					checkUserTable();
 					Login frame = new Login();
 					frame.setVisible(true);
 				} catch (Exception e) {
@@ -44,7 +50,7 @@ public class Login extends JFrame {
 	public Login() {
 		setBackground(new Color(255, 255, 255));
 		setResizable(false);
-		setTitle("Stock Management System");
+		setTitle("Stock Management System - Login");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 662, 382);
 		contentPane = new JPanel();
@@ -99,7 +105,11 @@ public class Login extends JFrame {
 		
 		JButton btnLogin = new JButton("LOG IN");
 		btnLogin.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {login();}
+			public void actionPerformed(ActionEvent e) {
+				String username = fieldUserName.getText();
+				String pass = passwordHash(passwordField.getText());
+				loginAction(username,pass);
+			}
 		});
 		btnLogin.setFont(new Font("SansSerif", Font.BOLD, 15));
 		btnLogin.setForeground(new Color(255, 255, 255));
@@ -114,19 +124,86 @@ public class Login extends JFrame {
 		contentPane.add(lblManagementSystem);
 	}
 	
-	public void login() {
-		DbConnection con = new DbConnection();
+	public void loginAction(String username, String password) {
 		try {			
+			PreparedStatement pst;
+			System.out.println("user = " + username);
+			System.out.println("password = " + password);
+			
+			String query = "SELECT password FROM users WHERE user_name='"+ username +"'";
+			pst = con.getConnection().prepareStatement(query);
+			ResultSet rs = pst.executeQuery();
+			
+			if (rs.next()) {
+				String pass = rs.getString("password");
+				if (pass.equals(password)){
+					this.dispose();
+					dashboard.setVisible(true);
+					System.out.println("Success");
+				}
+				else {
+					System.out.println("Incorrect password");
+				}
+			}
+			else {
+				System.out.println("User not found");
+			}
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+	}
+	
+	public static void checkUserTable() {
+		try {
+			DbConnection con = new DbConnection();
+			PreparedStatement pst;
 			Statement st = con.getConnection().createStatement();
 			
-			String query = "CREATE TABLE " + this.fieldUserName.getText() + " (id INT PRIMARY KEY AUTO_INCREMENT,"
-					+ "name VARCHAR(255))";
+			String query = "CREATE TABLE IF NOT EXISTS users ("
+					+ "id INT PRIMARY KEY AUTO_INCREMENT,"
+					+ "user_name VARCHAR (255),"
+					+ "password VARCHAR (255),"
+					+ "status BOOLEAN NOT NULL DEFAULT FALSE,"
+					+ "created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP);";
+			String query2 = "SELECT * FROM users WHERE user_name='admin'";
+			pst = con.getConnection().prepareStatement(query2);
+			ResultSet rs = pst.executeQuery();
+			
+			if (rs.next()){
+				System.out.println("default user exists");
+			}
+			else {
+				String hashPw = passwordHash("admin");
+				String query3 = "INSERT INTO users(user_name,password)"
+						+ "VALUES ('admin', '"+ hashPw +"')";
+				st.execute(query3);
+			}
+			
+			
 			st.execute(query);
 			st.close();
 			con.getConnection().close();
 			
-		} catch (Exception e1) {
-			e1.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+	}
+	
+	public static String passwordHash(String pass) {
+		String hash_pw = "";
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA");
+			md.update(pass.getBytes());
+			byte [] rbt = md.digest();
+			StringBuilder sb = new StringBuilder();
+			
+			for (byte b: rbt) {
+				sb.append(String.format("%02x", b));
+			}
+			hash_pw = sb.toString();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return hash_pw;
 	}
 }
